2a592aead0d8f0af795aaa2dad40df22fafa03fb
Amha-Endrayes <36323983+Amha-Endrayes@users.noreply.github.com>
2022-05-17 16:48:26 +0300

    added inital ALX _printf commit

---
+++ test_code.c
@@ -0,0 +1,32 @@
+
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main() {
+
+ _printf("Hello, World!");
+
+//_printf("my single char is %c : ", ch);
+//_printf("my string is %s : ", test);
+
+
+
+
+  return 0;
+}
+
+/*
+
+  // set precision to 3 decimal places
+  printf("%.3f / %.3f = %.3f \n", a, b, a / b);
+
+  // set width to 5 digits with *
+  printf("Setting width %*c \n", 5, ch);
+
+  // get octal value of an integer
+  printf("Octal equivalent of %d is %o", num, num);
+
+*/
